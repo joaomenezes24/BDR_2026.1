68add8c3923d0d19cd30f9dddf482a8f6fae8e4f
joaocruzs <joao.cruz.jc@ufpi.edu.br>
2026-06-30 14:11:08 -0300

    fix: backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000"
+        "https://bdr-2026-1.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
