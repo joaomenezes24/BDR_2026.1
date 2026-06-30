ea6e8dd950d5bcb9396b61774885bbd0e15502b5
joaocruzs <joao.cruz.jc@ufpi.edu.br>
2026-06-30 14:00:01 -0300

    ffrontend caminho

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,10 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
-export async function apiFetch<T>(
-  endpoint: string
-): Promise<T> {
-
-  const response = await fetch(
-    `${API_URL}${endpoint}`
-  );
+export async function apiFetch<T>(endpoint: string): Promise<T> {
+  const response = await fetch(`${API_URL}${endpoint}`);
 
   if (!response.ok) {
-    throw new Error(
-      `Erro HTTP ${response.status}`
-    );
+    throw new Error(`Erro HTTP ${response.status}`);
   }
 
   return response.json();
